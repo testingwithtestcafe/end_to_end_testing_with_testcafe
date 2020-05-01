f4ed3cd737f39d36480194d6084762a47f707c66
Dennis Martinez <dennis@dennmart.com>
2020-05-01 15:14:07 +0900

    Add login test from Chapter 3

---
+++ login_test.js
@@ -0,0 +1,16 @@
+import { Selector } from "testcafe";
+  
+fixture("TeamYap Login")
+  .page("https://teamyap.app/login");
+  
+test("User with valid account can log in", async t => {
+  await t
+    .typeText("#email", "dennis@dennmart.com")
+    .typeText("#password", "teamyap123");
+  
+  await t.click("#login_submit");
+  
+  await t
+    .expect(Selector("#sidebar").innerText)
+    .contains("Dennis Martinez");
+});
