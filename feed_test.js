31a706402f38002489bde586922de8c06d9b6f9f
Dennis Martinez <dennis@dennmart.com>
2020-07-01 17:23:23 +0900

    Add Chapter 14 tests (Intercepting HTTP Requests)

--- feed_test.js
+++ feed_test.js
@@ -1,6 +1,48 @@
+import { RequestLogger, RequestMock } from "testcafe";
+
 import feedPageModel from "./models/feed_page_model";
 import manageDataPageModel from "./models/manage_data_page_model";
 import { adminUser } from "./helpers/roles";
+
+const commentLogger = RequestLogger({
+  url: /\/api\/posts\/(\d+)\/comments/,
+  method: "post"
+});
+
+const feedResponse = [
+  {
+    body: "This is the latest post shown on TeamYap",
+    createdAt: "2020-05-25T20:32:19.154+09:00",
+    recentPostComments: [],
+    user: {
+      name: "Dennis Martinez",
+      profilePicture: "/packs/media/images/icons/avatar-3a690de84b67b1fe12b4486951cda785.svg"
+    }
+  },
+  {
+    body: "My first post on TeamYap!",
+    createdAt: "2020-05-25T17:06:40.016+09:00",
+    recentPostComments: [],
+    user: {
+      name: "Jane Smith",
+      profilePicture: "/packs/media/images/icons/avatar-3a690de84b67b1fe12b4486951cda785.svg"
+    }
+  }
+];
+
+const feedMock = RequestMock()
+  .onRequestTo({
+    url: "https://teamyap.app/api/posts",
+    method: "get"
+  })
+  .respond(feedResponse, 200);
+
+const feedErrorMock = RequestMock()
+  .onRequestTo({
+    url: "https://teamyap.app/api/posts",
+    method: "get"
+  })
+  .respond(null, 500);
 
 fixture("TeamYap Feed")
   .page("https://teamyap.app/feed")
@@ -25,6 +67,7 @@ test
   });
 
 test
+  .requestHooks(commentLogger)
   .meta({ "priority": "high" })
   ("Logged-in user can comment on feed post", async t => {
     await feedPageModel.createNewPost("Can someone leave a comment?");
@@ -36,6 +79,47 @@ test
     await feedPageModel.createNewComment("Here's a comment!");
 
     await t
+      .expect(commentLogger.contains(request => request.response.statusCode === 201))
+      .ok();
+
+    await t
       .expect(feedPageModel.postComments.innerText)
       .contains("Here's a comment!");
   });
+
+
+
+test
+  .requestHooks(feedMock)
+  ("Feed section automatically loads posts from the server", async t => {
+    await t
+      .expect(feedPageModel.feedPosts.count)
+      .eql(2);
+
+    await t
+      .expect(feedPageModel.feedPosts.nth(0).innerText)
+      .contains("This is the latest post shown on TeamYap");
+
+    await t
+      .expect(feedPageModel.feedPosts.nth(1).innerText)
+      .contains("My first post on TeamYap!");
+  });
+
+test
+  .requestHooks(feedErrorMock)
+  ("Feed section shows an error if messages can't load", async t => {
+  await t
+    .expect(feedPageModel.feedError.exists)
+    .ok();
+
+  await t
+    .removeRequestHooks(feedErrorMock)
+    .addRequestHooks(feedMock)
+    .navigateTo("https://teamyap.app/feed");
+
+  await t
+    .expect(feedPageModel.feedError.exists)
+    .notOk()
+    .expect(feedPageModel.feedPosts.count)
+    .eql(2);;
+});
--- models/feed_page_model.js
+++ models/feed_page_model.js
@@ -4,7 +4,9 @@ class FeedPageModel {
 	constructor() {
 		this.postTextarea = Selector("#new_post_textarea");
 		this.postSubmitButton = Selector("#new_post_submit");
-		this.firstFeedPost = Selector("#feed_posts").nth(0);
+    this.firstFeedPost = Selector("#feed_posts").nth(0);
+    this.feedPosts = Selector(".feed-post");
+    this.feedError = Selector(".feed-error");
 
 		this.commentTextarea = this.firstFeedPost.find(".new-comment-textarea");
 		this.commentSubmitButton = this.firstFeedPost.find(".new-comment-submit");
